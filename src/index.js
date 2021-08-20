526fde85e3b9ea7452b61c8da06c49227c144279
Anya <annagapyuk@gmail.com>
2021-08-20 11:07:07 -0400

    displaying books dynamically

--- src/index.js
+++ src/index.js
@@ -3,35 +3,45 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 // setup vars
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/51YIcGCYtxL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-  title: "Divergent",
-  author: "Veronica Roth",
-};
+const books = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51YIcGCYtxL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Divergent",
+    author: "Veronica Roth",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51h4JPR8ZIL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Insurgent",
+    author: "Veronica Roth",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51WKe9aGJIL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Allegiant",
+    author: "Veronica Roth",
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>description here</p>
-      </Book>
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}
+      ;
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
-      {children}
     </article>
   );
 };
