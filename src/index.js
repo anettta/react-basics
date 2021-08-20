c975970c0be6fa4b59556739e2c9d36314f8cc79
Anya <annagapyuk@gmail.com>
2021-08-20 16:50:16 -0400

    added event handler

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import "./index.css";
 // setup vars
 const books = [
   {
+    id: 1,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/51YIcGCYtxL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "Divergent",
     author: "Veronica Roth",
   },
   {
+    id: 2,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/51h4JPR8ZIL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "Insurgent",
     author: "Veronica Roth",
   },
   {
+    id: 3,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/51WKe9aGJIL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "Allegiant",
@@ -28,20 +31,28 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
       ;
     </section>
   );
 };
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  // attribut, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world");
+  };
   return (
     <article className="book">
       <img src={img} alt="" />
+
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
+      <button type="button" onClick={clickHandler}>
+        ref ex
+      </button>
     </article>
   );
 };
